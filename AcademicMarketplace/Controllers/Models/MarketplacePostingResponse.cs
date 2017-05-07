40805e1d3d7c5892e71d98b653d3b11dfc6c278e
taulelei <isaac.taulelei@gmail.com>
2017-05-07 16:35:17 +0930

    Added solution folder structure and projects for N-tier architecture. Added basic Models to Infrastructure and App layer, to be improved on until we determine DB structure. Added T4TS to App layer for generating Typescript interface definitions from the App layer C# models.

---
+++ AcademicMarketplace/Controllers/Models/MarketplacePostingResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using T4TS;
+
+namespace AcademicMarketplace.Controllers.Models
+{
+    [TypeScriptInterface]
+    public class MarketplacePostingResponse
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string PostContent { get; set; }
+        public DateTime PostDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime CompletedDate { get; set; }
+        public int CompletedBy { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ Infrastructure.AcademicMarketplace/Models/MarketplacePostingRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Infrastructure.AcademicMarketplace.Models
+{
+    public class MarketplacePostingRequest
+    {
+        public int PostId { get; set; }
+        public string PostTitle { get; set; }
+        public int PostedBy { get; set; }
+
+    }
+}
