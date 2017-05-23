694714bbb9e4a1fa15e9ab019b36215b4f36e1a5
Phil Davis <phil@jankaritech.com>
2017-05-23 22:06:48 +0545

    j

--- travis-build.sh
+++ travis-build.sh
@@ -5,6 +5,7 @@ ls -l /usr/share/dotnet/sdk/1.0.0-rc4-004771/Microsoft
 ls -l /usr/share/dotnet/sdk/1.0.0-rc4-004771
 ls -l /usr/share/dotnet/sdk
 ls -l /usr/share/dotnet
+find / -name Microsoft.WebApplication.targets
 echo "Doing dotnet restore"
 dotnet restore
 echo "Doing dotnet build"
