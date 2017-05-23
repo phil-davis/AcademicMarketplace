97f65c32db350a5bfa2ef20420f1ef8f8ce8ab9f
Phil Davis <phil@jankaritech.com>
2017-05-23 21:29:15 +0545

    d

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 pwd
 ls -l /usr/share/dotnet/sdk/1.0.4/Microsoft/VisualStudio
+ls -l /usr/share/dotnet/sdk/1.0.4/Microsoft
+ls -l /usr/share/dotnet/sdk/1.0.4
+ls -l /usr/share/dotnet/sdk
+ls -l /usr/share/dotnet
 echo "Doing dotnet restore"
 dotnet restore
 echo "Doing dotnet build"
