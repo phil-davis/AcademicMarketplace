6148063992366fb58bdf40cda42c743d91406251
Phil Davis <phil@jankaritech.com>
2017-05-25 14:49:20 +0545

    First xml2js code
    Signed-off-by: Phil Davis <phil@jankaritech.com>

--- api_tests/api_basic_tests_spec.js
+++ api_tests/api_basic_tests_spec.js
@@ -1,4 +1,5 @@
 var frisby = require('frisby');
+var xml2js = require('xml2js');
 
 // Test that the Home/Index endpoint exists
 // This should redirect to the login page, returning html
@@ -7,7 +8,30 @@ var frisby = require('frisby');
 // port.
 
 frisby.create('Academic Marketplace basic site response test')
-  .get('http://academicmarketplace.azurewebsites.net/Home/Index')
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'text/html')
+	.get('http://academicmarketplace.azurewebsites.net/Home/Index')
+	.expectStatus(200)
+	.expectHeaderContains('content-type', 'text/html')
 .toss();
+
+frisby.create('Workgroup GetAll test')
+	.get('http://academicmarketplace.azurewebsites.net/Workgroup/GetAll/')
+	.expectHeaderContains('content-type', 'text/xml')
+	.after(function (err, res, body) {
+		var parser = new xml2js.Parser();
+		parser.parseString(body, function (err, result) {
+			expect(result).toContainJson({
+				"server-auth": {
+					"$": {
+						"enabled":"true"
+					},
+					"ldapAuth":["false"],
+					"emailAuth":["true"]
+				}
+			});
+			expect(result).expectJSONTypes({
+				ArrayOfWorkgroupModel: Array
+			});
+		});
+	})  
+.toss();
+	
