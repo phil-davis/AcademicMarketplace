0a5987d6807fd2bb843c46abeaf6a99ceec3bc63
Phil Davis <phil@jankaritech.com>
2017-05-25 16:44:00 +0545

    post 3
    Signed-off-by: Phil Davis <phil@jankaritech.com>

--- api_tests/api_basic_tests_spec.js
+++ api_tests/api_basic_tests_spec.js
@@ -31,7 +31,7 @@ frisby.create('Workgroup GetAll test')
 	.post('http://academicmarketplace.azurewebsites.net/Workgroup/GetUserWorkgroups',
 		{ username: 'test' })
 	.auth('test', 'Tester!2', false)
-	.expectStatus(200)
+	.expectStatus(302)
 	.expectHeaderContains('content-type', 'application/json')
 	.inspectJSON()
 .toss();
